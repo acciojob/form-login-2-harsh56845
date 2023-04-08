8c4b0df74dfc5c21ba7cd9c17ac1aeaf77995af5
harsh56845 <56841891+harsh56845@users.noreply.github.com>
2023-04-09 00:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function myFunction() {
+	var fn = document.getElementById("fn").value;
+	var ln = document.getElementById("ln").value;
+	var pn = document.getElementById("pn").value;
+	var ei = document.getElementById("ei").value;
+	
+  alert("First Name: "+fn+"\n"+"Last Name: "+ln+"\n"+"Phone no: "+pn+"\n"+"Email ID: "+ei);
+
+}
